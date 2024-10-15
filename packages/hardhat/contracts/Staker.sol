fa2d59411cf2bebce260edb1671f447a9c802777
Sandra Carrillo <sancarrillo411@gmail.com>
2024-10-15 09:46:10 -0500

    challenge 1 done

--- packages/hardhat/contracts/Staker.sol
+++ packages/hardhat/contracts/Staker.sol
@@ -8,24 +8,58 @@ contract Staker {
 
   ExampleExternalContract public exampleExternalContract;
 
+  mapping ( address => uint256 ) public balances;
+  uint256 public constant threshold = 1 ether;
+  uint256 public deadline = block.timestamp + 72 hours;
+
+  bool public openForWithdraw = false;
+
+  event Stake(address, uint256);
+
   constructor(address exampleExternalContractAddress) {
       exampleExternalContract = ExampleExternalContract(exampleExternalContractAddress);
   }
 
   // Collect funds in a payable `stake()` function and track individual `balances` with a mapping:
   // (Make sure to add a `Stake(address,uint256)` event and emit it for the frontend `All Stakings` tab to display)
+  
+  function stake() public payable {
+    balances[msg.sender] += msg.value;
+    emit Stake(msg.sender, msg.value);
+  }
 
 
   // After some `deadline` allow anyone to call an `execute()` function
   // If the deadline has passed and the threshold is met, it should call `exampleExternalContract.complete{value: address(this).balance}()`
 
+  function execute() public {
+    require(block.timestamp > deadline, "Deadline not met");
+    if (address(this).balance >= threshold) {
+      exampleExternalContract.complete{value: address(this).balance}();
+      openForWithdraw = true;
+    }
+  }
 
   // If the `threshold` was not met, allow everyone to call a `withdraw()` function to withdraw their balance
-
+  function withdraw() public {
+    require(block.timestamp > deadline || openForWithdraw, "Deadline not met");
+    uint256 balance = balances[msg.sender];
+    require(balance > 0, "No balance to withdraw");
+    balances[msg.sender] = 0;
+    payable(msg.sender).transfer(balance);
+  }
 
   // Add a `timeLeft()` view function that returns the time left before the deadline for the frontend
+  function timeLeft() public view returns (uint256) {
+    if (block.timestamp >= deadline) {
+      return 0;
+    }
+    return deadline - block.timestamp;
+  }
 
-
-  // Add the `receive()` special function that receives eth and calls stake()
+  // Add the `receive()` special function that receives eth and calls stake(). Use the receive() function in solidity to "catch" ETH sent to the contract and call stake() to update balances.
+  receive() external payable {
+    stake();
+  }
 
 }
